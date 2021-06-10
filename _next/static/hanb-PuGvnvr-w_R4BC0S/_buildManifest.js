5cb2daf5ec3b43c6e718e3846131ad3f4b144e5d
Cédric <cedric.demongivert@gmail.com>
2021-06-10 09:21:47 -0400

    update

---
+++ _next/static/hanb-PuGvnvr-w_R4BC0S/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-7553a2e0dce452f2b3b7.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a574d65f856309ef3c5d.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
